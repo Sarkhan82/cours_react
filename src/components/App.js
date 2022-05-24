a9a2fc971eff3fd5ceb1599cb49b594318c0bf5a
Sarkhan <david.jonquieres@gmail.com>
2022-05-24 19:14:29 +0200

    1er commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import Banner from "./Banner";
+import Cart from "./Cart";
+
+function App() {
+  return (
+    <div className="App">
+      <Banner /> <Cart />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Banner.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/Banner.css";
+
+const Banner = () => {
+  const title = "La maison de la Jungle";
+  return (
+    <div className="lmj-banner">
+      <h1> {title} </h1>
+    </div>
+  );
+};
+
+export default Banner;
